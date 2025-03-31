4ad076f26ec41d8ee045d8c182a31ff793966957
Jakub Mitický <145475310+jmiticky4130@users.noreply.github.com>
2025-03-31 16:21:54 +0200

    Update login-form.js

--- src/app/components/login-form.js
+++ src/app/components/login-form.js
@@ -98,7 +98,7 @@ export default function LoginForm() {
       </div>
 
       <div className="text-center text-sm">
-        Don't have an account?{' '}
+        Don&apos;t have an account?{' '}
         <Link href="/register" className="text-blue-600 hover:underline">
           Create an account
         </Link>
